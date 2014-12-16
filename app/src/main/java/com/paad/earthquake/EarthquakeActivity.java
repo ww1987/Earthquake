c0b57e4338a5a7630edc129cdb06d2b9498e51fe
ww1987 <overlink@163.com>
2014-12-16 21:19:57 +0800

    添加保存sharpPerference

--- app/src/main/java/com/paad/earthquake/EarthquakeActivity.java
+++ app/src/main/java/com/paad/earthquake/EarthquakeActivity.java
@@ -1,12 +1,19 @@
 package com.paad.earthquake;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class EarthquakeActivity extends Activity {
+
+    private static final int MENU_PREFERENCE = Menu.FIRST+1;
+    private static final int SHOW_PREFERENCE = 1;
+    private static final int MENU_UPDATE = Menu.FIRST+2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +25,23 @@ public class EarthquakeActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_earthquake, menu);
+        super.onCreateOptionsMenu(menu);
+        menu.add(0,MENU_PREFERENCE,Menu.NONE,R.string.menu_preferences);
         return true;
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case (MENU_PREFERENCE) : {
+                Intent i = new Intent(this,PreferencesActivity.class);
+                startActivityForResult(i,SHOW_PREFERENCE);
+                return true;
+            }
         }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
--- app/src/main/java/com/paad/earthquake/PreferencesActivity.java
+++ app/src/main/java/com/paad/earthquake/PreferencesActivity.java
@@ -1,7 +1,14 @@
 package com.paad.earthquake;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 
@@ -13,18 +20,83 @@ public class PreferencesActivity extends Activity {
     private CheckBox autoUpdate;
     private Spinner updateFreqSpinner;
     private Spinner magnitudeSpinner;
+
+    private static final String USER_PREFERENCE = "USER_PREFERENCE";
+    private static final String PREF_AUTO_UPDATE = "PREF_AUTO_UPDATE";
+    private static final String PREF_MIN_MAG_INDEX = "PREF_MIN_MAG_INDEX";
+    private static final String PREF_UPDATE_FREQ_INDEX = "PREF_UPDATE_FREQ_INDEX";
+
+    SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preferences);
 
+
         autoUpdate = (CheckBox) findViewById(R.id.checkbox_auto_update);
         updateFreqSpinner = (Spinner) findViewById(R.id.spinner_update_freq);
         magnitudeSpinner = (Spinner) findViewById(R.id.spinner_quake_mag);
         populateSpinners();
+
+        Context context= getApplicationContext();
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        updateUIFromPreference();
+
+        Button okButton = (Button) findViewById(R.id.okButton);
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePreference();
+                PreferencesActivity.this.setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PreferencesActivity.this.setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+    }
+
+    private void savePreference() {
+        int updateIndex = updateFreqSpinner.getSelectedItemPosition();
+        int minMagIndex = magnitudeSpinner.getSelectedItemPosition();
+        boolean autoUpdateChecked =  autoUpdate.isChecked();
+
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putBoolean(PREF_AUTO_UPDATE,autoUpdateChecked);
+        edit.putInt(PREF_UPDATE_FREQ_INDEX,updateIndex);
+        edit.putInt(PREF_MIN_MAG_INDEX,minMagIndex);
+        edit.apply();
+    }
+
+    private void updateUIFromPreference() {
+        boolean autoUpchecked = prefs.getBoolean(PREF_AUTO_UPDATE,false);
+        int updateFreqIndex = prefs.getInt(PREF_UPDATE_FREQ_INDEX,2);
+        int minMagIndex = prefs.getInt(PREF_MIN_MAG_INDEX,0);
+        updateFreqSpinner.setSelection(updateFreqIndex);
+        magnitudeSpinner.setSelection(minMagIndex);
+        autoUpdate.setChecked(autoUpchecked);
     }
 
     private void populateSpinners() {
+        //填充更新频率为条框
+        ArrayAdapter<CharSequence> fAdapter;
+        fAdapter = ArrayAdapter.createFromResource(this,R.array.update_freq_options,android.R.layout.simple_spinner_item);
+        int spinner_dd_item = android.R.layout.simple_spinner_dropdown_item;
+        fAdapter.setDropDownViewResource(spinner_dd_item);
+        updateFreqSpinner.setAdapter(fAdapter);
+        //填充最小甄姬微调框
+        ArrayAdapter<CharSequence> mAdapter;
+        mAdapter = ArrayAdapter.createFromResource(this,R.array.magnitude_options,android.R.layout.simple_spinner_item);
+        mAdapter.setDropDownViewResource(spinner_dd_item);
+        magnitudeSpinner.setAdapter(mAdapter);
 
     }
 }
